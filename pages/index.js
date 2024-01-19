6a9db3735de70442308d9dd8b2c0e2be9743c7b4
marcfelipe <marcfelipe@gmail.com>
2024-01-19 14:49:16 +0000

    added prettier configuration and scripts `lint:check` `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home(){
-    return <h1>Mamãe, decide o meu nome. Sugestão do papai e da Lala é Luckinhas</h1>
+function Home() {
+  return (
+    <h1>Mamãe, decide o meu nome. Sugestão do papai e da Lala é Luckinhas</h1>
+  );
 }
 
 export default Home;
